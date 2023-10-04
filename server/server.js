5c0d0b7382e0f597c2719d05b5ebc75d5e90b291
Amelia Hill <amehi4yus@gmail.com>
2023-10-03 19:05:28 -0700

    First commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const PORT = 5000
+
+app.use(cors())
+
+app.get("/api/home", (req, res)=> {
+    res.json({message: "Well, hello there gorgeous!"})
+})
+
+app.listen(PORT, ()=>{ console.log(`Running on port ${PORT}`)})
