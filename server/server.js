4016c943ea393bd48379bb7cc4f88b6626b737c6
Amelia Hill <amehi4yus@gmail.com>
2023-10-03 19:42:14 -0700

    Integrated client directory into main repository

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const PORT = 5000
 app.use(cors())
 
 app.get("/api/home", (req, res)=> {
-    res.json({message: "Well, hello there gorgeous!"})
+    res.json({message: "Hello there gorgeous!"})
 })
 
 app.listen(PORT, ()=>{ console.log(`Running on port ${PORT}`)})
