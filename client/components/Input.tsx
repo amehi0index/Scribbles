bee3754955c276440e7e229fc6887f1c396dd8fd
Amelia Hill <amehi4yus@gmail.com>
2023-12-04 14:28:47 -0800

    Set up database connection

--- client/components/Input.tsx
+++ client/components/Input.tsx
@@ -7,7 +7,7 @@ const Input: React.FC<InputProps> = (props) => {
   return (
     <input dir="ltr"
       {...props} 
-      className={`outline-none bg-white text-gray-400 lg:text-lg sm:text-md opacity-75 border-gray-300 border rounded-s-lg pl-2 w-4/5 focus:shadow-inputfocus focus:border-white ${props.className}`} 
+      className={`outline-none bg-white text-gray-400 lg:text-lg sm:text-md opacity-75 border-gray-300 border rounded-s-lg pl-2 w-3/5 focus:shadow-inputfocus focus:border-white ${props.className}`} 
       autoComplete="false" 
     />
   )
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const client = require('./connection.js')
 const express = require("express")
 const app = express()
 const cors = require("cors")
@@ -6,15 +7,35 @@ const PORT = 5000
 app.use(cors())
 app.use(express.json())
 
+client.connect()
+
 app.get("/api/home", (req, res)=> {
     res.json({message: "Hello from the backend. *wink *wink"})
+});
+
+app.get('/api/email-subscribers', (req, res) => {
+    client.query('SELECT * FROM email_subs', (err, result) => {
+        if (err) {
+            console.error(err.message);
+            res.status(500).json({ error: "Internal server error" });
+        } else {
+            res.json(result.rows);
+        }
+    });
+});
+
+app.post('/api/sendemail', (req, res)=> {
+    const { email } = req.body;
+    let insertQuery = `INSERT INTO email_subs(email_sub) 
+    VALUES($1)`
+    client.query(insertQuery, [email], (err, result)=>{
+        if(!err){
+            res.status(200).json({ message: 'Thanks! We got your little email.' });
+        }
+        else{ console.log(err.message) }
+    })
+    // client.end;
 })
 
-app.post('/api/sendemail', (req, res) => {
-    const { email } = req.body;
-    console.log('Received email:', email);
-
-    res.status(200).json({ message: 'Thanks! We got your little email.' });
-});
 
 app.listen(PORT, ()=>{ console.log(`Running on port ${PORT}`)})
