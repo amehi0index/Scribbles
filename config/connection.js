43a1db9ab36ee962828b0c62c21027710e3556d5
Amelia Hill <amehi4yus@gmail.com>
2023-12-06 20:23:18 -0800

    Update db config

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,27 @@
 require('dotenv').config();
-const { Client } = require('pg');
+const { Pool } = require('pg');
 
-const client = new Client({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    port: process.env.DB_PORT,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
-});
+let dbConfig;
+
+if (process.env.NODE_ENV === "production") {
+    // On Heroku, use the DATABASE_URL environment variable
+    dbConfig = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false // Required for Heroku's self-signed certificate
+        }
+    };
+} else {
+    // In development, use local environment variables
+    dbConfig = {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    };
+}
+
+const client = new Pool(dbConfig);
 
 module.exports = client;
