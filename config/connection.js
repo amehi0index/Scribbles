8db9b178a2e181b1425b55fe06977f001643c885
Amelia Hill <amehi4yus@gmail.com>
2023-12-06 21:15:07 -0800

    Configure helmet

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,39 @@
 require('dotenv').config();
-// const { Client } = require('pg');
-
-// const client = new Client({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USER,
-//     port: process.env.DB_PORT,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DB_DATABASE
-// });
-
-// module.exports = client;
-
-
 const { Pool } = require('pg');
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+let dbConfig;
 
-module.exports = pool;
+if (process.env.NODE_ENV === "production") {
+    // On Heroku, use the DATABASE_URL environment variable
+    dbConfig = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false // Required for Heroku's self-signed certificate
+        }
+    };
+} else {
+    // Development
+    dbConfig = {
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        port: process.env.DB_PORT,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
+    };
+}
+
+const client = new Pool(dbConfig);
+
+module.exports = client;
+
+
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
+
+// module.exports = pool;
--- server.js
+++ server.js
@@ -3,11 +3,19 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 const path = require ('path')
+const helmet = require ('helmet')
 require('dotenv').config()
 const PORT = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
+
+app.use(helmet.contentSecurityPolicy({
+    directives: {
+        defaultSrc: ["'none'"],
+        imgSrc: ["'self'", "data:"]
+    }
+}))
 
 
 //DEFINE ROUTES
