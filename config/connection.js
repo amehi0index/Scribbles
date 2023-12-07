784fbb68543a9681f9b5245bf7504df4f09d4c72
Amelia Hill <amehi4yus@gmail.com>
2023-12-06 20:50:31 -0800

    Test deployment config

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,38 @@
 require('dotenv').config();
-const { Pool } = require('pg');
+// const { Pool } = require('pg');
 
-let dbConfig;
+// let dbConfig;
 
-if (process.env.NODE_ENV === "production") {
-    // On Heroku, use the DATABASE_URL environment variable
-    dbConfig = {
-        connectionString: process.env.DATABASE_URL,
-        ssl: {
-            rejectUnauthorized: false // Required for Heroku's self-signed certificate
-        }
-    };
-} else {
-    // In development, use local environment variables
-    dbConfig = {
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        port: process.env.DB_PORT,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE
-    };
-}
+// if (process.env.NODE_ENV === "production") {
+//     // On Heroku, use the DATABASE_URL environment variable
+//     dbConfig = {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: {
+//             rejectUnauthorized: false // Required for Heroku's self-signed certificate
+//         }
+//     };
+// } else {
+//     // In development, use local environment variables
+//     dbConfig = {
+//         host: process.env.DB_HOST,
+//         user: process.env.DB_USER,
+//         port: process.env.DB_PORT,
+//         password: process.env.DB_PASSWORD,
+//         database: process.env.DB_DATABASE
+//     };
+// }
 
 const client = new Pool(dbConfig);
+
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // Required for Heroku's self-signed certificate
+    }
+});
+
+module.exports = pool;
 
 module.exports = client;
