4a0d624f9fbb0e1eba284c9fd58b53012ae65660
Amelia Hill <amehi4yus@gmail.com>
2023-12-06 19:02:26 -0800

    Prepare for deployment

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const path = require ('path')
+require('dotenv').config()
 const PORT = 5000
 
 app.use(cors())
@@ -14,6 +16,16 @@ app.use('/api/email-subscribers', require('./routes/subscribers'))
 app.get("/api/home", (req, res)=> {
     res.json({message: "Hello from the backend. *wink *wink"})
 });
+
+//SERVE STATIC ASSETS IN PRODUCTION?
+if(process.env.NODE_ENV === 'production'){
+
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {  
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')) //
+    })
+}
 
 
 app.listen(PORT, ()=>{ console.log(`Running on port ${PORT}`)})
