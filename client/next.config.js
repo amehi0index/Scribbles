fc6bf30b0d1680db85f69aa4b4ab604c1113d93a
Amelia Hill <amehi4yus@gmail.com>
2023-12-07 19:21:56 -0800

    Next config

--- client/next.config.js
+++ client/next.config.js
@@ -1,17 +1,31 @@
+/** @type {import('next').NextConfig} */
 const isDevelopment = process.env.NODE_ENV !== 'production';
 
 const nextConfig = {
-  async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `default-src 'self'; img-src 'self' data: https://scribbles-dac22275e7f8.herokuapp.com; script-src 'self' ${isDevelopment ? "'unsafe-eval'" : ""} https://scribbles-dac22275e7f8.herokuapp.com; style-src 'self' 'unsafe-inline';`,
-          },
-        ],
-      },
-    ]
-  },
+    async headers() {
+        return [
+            {
+                source: '/api/email-subscribers/new',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: isDevelopment
+                            ? "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'self' http://localhost:5000;"
+                            : "default-src 'self'; img-src 'self' https://scribbles-dac22275e7f8.herokuapp.com; script-src 'self'; style-src 'self'; connect-src 'self' https://scribbles-dac22275e7f8.herokuapp.com;",
+                    },
+                ],
+            },
+            {
+                source: '/:path*',
+                headers: [
+                    {
+                        key: 'Content-Security-Policy',
+                        value: isDevelopment
+                            ? "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; connect-src 'self' http://localhost:5000;"
+                            : "default-src 'self'; img-src 'self' https://scribbles-dac22275e7f8.herokuapp.com; script-src 'self'; style-src 'self'; connect-src 'self' https://scribbles-dac22275e7f8.herokuapp.com;",
+                    },
+                ],
+            },
+        ];
+    },
 };
